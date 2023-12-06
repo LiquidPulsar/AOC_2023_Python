5a4f591a32a4d89e8a54d39b89c841bc9a39dc15
Alex <noelalexandre66@gmail.com>
2023-12-06 00:27:55 +0000

    Treating myself to a big refactor

--- Day_5/p1.py
+++ Day_5/p1.py
@@ -6,48 +6,31 @@ from more_itertools import chunked
 
 HOME = Path(__file__).parent
 
-from collections import defaultdict
-
 def location(seed):
-    steps = "seed soil fertilizer water light temperature humidity location".split()
-    s = iter(steps)
-    a = next(s)
-    for b in s:
-        # print(seed,a,b)
-        seed = maps[a][b][seed]
-        a = b
-        # break
+    for _map in maps:
+        seed = _map[seed]
     return seed
 
 @dataclass
 class RangeMap:
     ranges: list[tuple[int,int,int]] = field(default_factory=list)
     
-    def setrange(self,k,v,r):
-        insort_left(self.ranges,(k,v,r))
+    def setrange(self,k,v,r): insort_left(self.ranges,(k,v,r))
     
     def __getitem__(self,k):
         i = bisect_right(self.ranges,(k,float("inf"),0))
-        # print(self.ranges,(k,0,0),i)
         if i == 0: return k
-        (l,v,r) = self.ranges[i-1]
-        return v+(k-l) if l<=k<l+r else k
+        (l,r,d) = self.ranges[i-1]
+        return k + d*(l<=k<r)
 
-maps = defaultdict(lambda : defaultdict(RangeMap))
-
+maps:list[RangeMap] = []
 with open(HOME/"input.txt") as f:
     seeds = [*map(int,f.readline().split(": ",1)[1].split())]
     f.readline()
-    for part in f.read().split("\n\n"):
-        part = part.splitlines()
-        src,dest = part[0].removesuffix(" map:").split("-to-")
+    for part in map(str.splitlines,f.read().split("\n\n")):
+        maps.append(RangeMap())
         for line in part[1:]:
             a,b,c = map(int,line.split())
-            maps[src][dest].setrange(b,a,c)
-    # print(maps["seed"]["soil"])
+            maps[-1].setrange(b,b+c,a-b) # kl,kr, delta from left to right
+
     print(min(map(location,seeds)))
-    # sds = []
-    # for l,s in chunked(seeds,2): sds.extend(range(l,l+s))
-    # print(sds)
-    # print(*map(location,sds))
-    # print(min(map(location,sds)))
--- Day_5/p2.py
+++ Day_5/p2.py
@@ -1,41 +1,24 @@
+from itertools import starmap, chain
+from functools import reduce
 from dataclasses import dataclass, field
 from bisect import insort_left, bisect_right
-from more_itertools import chunked
 from pathlib import Path
 
+INF = int(1e10)
 HOME = Path(__file__).parent
-
-from collections import defaultdict
-
-steps = "seed soil fertilizer water light temperature humidity location".split()
-
-def locations(seeds):
-    s = iter(steps)
-    a = next(s)
-    for b in s:
-        nxt_seeds = []
-        for seed in seeds:
-            nxt_seeds.extend(maps[a][b].checked(*seed))
-        seeds = nxt_seeds
-        a = b
-    return seeds
 
 @dataclass
 class RangeMap:
-    # kleft,kright,vleft | kright exclusive
+    # kleft,kright,delta | kright exclusive
     ranges: list[tuple[int,int,int]] = field(default_factory=list)
     
-    def setrange(self,kl,kr,vl):
-        insort_left(self.ranges,(kl,kr,vl))
-    
-    def find_left(self,k):
-        return bisect_right(self.ranges,(k,float('inf'),0))
+    def setrange(self,kl,kr,vl): insort_left(self.ranges,(kl,kr,vl))
 
-    def get_overlaps(self,kl,kr):
+    def get_overlaps(self,kl,kr) -> list[tuple[int,int]]:
         assert kr>kl
-        if not self.ranges: return [(kl,kr)] # Nothing to overlap
-        i = self.find_left(kl)
-        (_,prv_kr,prv_d) = self.ranges[i-1] if i else (float('-inf'),float('-inf'),0)
+
+        i = bisect_right(self.ranges,(kl,float('inf'),0))
+        (_,prv_kr,prv_d) = self.ranges[i-1] if i else (-INF,-INF,0)
         if prv_kr<=kl:
             if i == len(self.ranges): return [(kl,kr)] # off the right side
             nxt_kl = self.ranges[i][0]
@@ -44,103 +27,30 @@ class RangeMap:
                 if nxt_kl >= kr
                 else [(kl, nxt_kl)] + self.get_overlaps(nxt_kl, kr)
             )
-        # prv_kl known <= kl
 
-        if prv_kr<kr:
+        if prv_kr<kr: # prv_kl known <= kl
             return [translate((kl,prv_kr),prv_d)] + self.get_overlaps(prv_kr,kr)
         else:
             return [translate((kl,kr),prv_d)]
     
-    def checked(self,kl,kr):
-        res = self.get_overlaps(kl,kr)
-        res2 = self.worse([(kl,kr)])
-        if set(res) != set(res2):
-            print("========== ERROR ==========")
-            print(kl,kr,self.ranges)
-            print("res",res)
-            print("res2",res2)
-            print(set(res) - set(res2))
-            print(set(res2) - set(res))
-        assert set(res) == set(res2), f'{set(res) - set(res2)} {set(res2) - set(res)}'
-        return res
+def translate(ab:tuple[int,int],c:int) -> tuple[int,int]:
+    return (ab[0]+c,ab[1]+c)
 
-    
-    def worse(self,seeds):
-        seeds = seeds.copy()
-        out = []
-        while seeds:
-            s, e = seeds.pop()
-            for kl,kr,d in self.ranges:
-                ll = max(kl,s)
-                rr = min(kr,e)
-                if ll<rr:
-                    out.append((ll+d,rr+d))
-                    if s < ll:
-                        seeds.append((s,ll))
-                    if rr < e:
-                        seeds.append((rr,e))
-                    break
-            else:
-                out.append((s,e))
-        return out
+def pipeline(lst:list[tuple[int,int]],_map:RangeMap):
+    return chain.from_iterable(starmap(_map.get_overlaps, lst))
 
-def translate(ab,c):
-    (a,b) = ab
-    return (a+c,b+c)
+def locations(seeds:list[tuple[int,int]]):
+    return reduce(pipeline,maps,seeds) # type: ignore
 
-# m = RangeMap()
 
-# m.setrange(0,37,15)
-# m.setrange(37,39,52)
-# m.setrange(39,54,0)
-
-# print(m.get_overlaps(-1,0))
-
-def fails(l,r):
-    try:
-        maps["seed"]["soil"].checked(l,r)
-        return False
-    except AssertionError:
-        print("Failed",l,r)
-        return True
-
-maps = defaultdict(lambda : defaultdict(RangeMap))
-
+maps:list[RangeMap] = []
 with open(HOME/"input.txt") as f:
     seeds = [*map(int,f.readline().split(": ",1)[1].split())]
     f.readline()
-    for part in f.read().split("\n\n"):
-        part = part.splitlines()
-        src,dest = part[0].removesuffix(" map:").split("-to-")
+    for part in map(str.splitlines,f.read().split("\n\n")):
+        maps.append(RangeMap())
         for line in part[1:]:
             a,b,c = map(int,line.split())
-            maps[src][dest].setrange(b,b+c,a-b) # kl,kr, delta from left to right
-    
-    # l,r = 635790399,1341769649
-    # gap = r-l
-    # while gap:
-    #     print(l,r,gap)
-    #     gap //= 2
-    #     if fails(l+gap,r):
-    #         l += gap
-    #     elif fails(l,r-gap):
-    #         r -= gap
-    #     else:
-    #         print("ERROR")
-    #         break
+            maps[-1].setrange(b,b+c,a-b) # kl,kr, delta from left to right
 
-    # 768672890 768672903
-    # (629314747, 762725199, -360088562), (768672891, 808199470, 2401051598)
-
-    # print(maps["seed"]["soil"].ranges)
-    # maps["seed"]["soil"].checked(768672890, 768672903)
-
-    # print(maps["soil"]["fertilizer"])
-    # RangeMap(ranges=[(0, 37, 15), (37, 39, 52), (39, 54, 0)])
-    # print(maps["soil"]["fertilizer"].get_overlaps(53,67))
-    # print(min(map(location,seeds)))
-    # print([(l,l+s) for l,s in chunked(seeds,2)])
-    res = locations([(l,l+s) for l,s in chunked(seeds,2)])
-    # print(res)
-    print(min(map(lambda x: x[0],res)))
-    # print(sorted(res,key=lambda x: x[0]))
+    print(min(map(lambda x: x[0],locations([(l,l+s) for l,s in zip(seeds[::2],seeds[1::2])]))))
