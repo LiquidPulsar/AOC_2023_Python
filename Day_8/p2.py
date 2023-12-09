5cba3116bc25251a87cd0c85881163a22282723a
Alex <noelalexandre66@gmail.com>
2023-12-09 04:02:36 +0000

    Day 8 done: disappointing part 2

--- Day_8/p2.py
+++ Day_8/p2.py
@@ -1,43 +1,37 @@
 from itertools import cycle
+import math
 from pathlib import Path
 import re
-import numpy as np
 
 HOME = Path(__file__).parent
 
 left = {}
 right = {}
 sources = []
-targets= []
 with open(HOME/"input.txt") as f:
-    dirs = cycle(f.readline().strip())
+    dirs = f.readline().strip()
     f.readline()
-    names = {}
-    n = 0
     for k,l,r in re.findall(
         r"(\w+) = \((\w+), (\w+)\)",
         f.read()
         ):
-        for i in (k,l,r):
-            if i not in names:
-                names[i] = n
-                if i.endswith("A"):
-                    sources.append(n)
-                elif i.endswith("Z"):
-                    targets.append(n)
-                n += 1
-        left[names[k]] = names[l]
-        right[names[k]] = names[r]
+        if k.endswith("A"):
+            sources.append(k)
+        left[k] = l
+        right[k] = r
     
-    sources = [i for i in range(n) if i in sources]
-    sources = np.array(sources)
-
-    targets = [i for i in range(n) if i in targets]
-    targets = np.array(targets)
-    
-    for step,dir in enumerate(dirs,1):
-        # print(step,currs,dir)
-        currs = [dct[curr][dir=='R'] for curr in currs]
-        if all(c.endswith("Z") for c in currs):
-            print(step,time()-t)
-            break
+    lcm = 1
+    for curr in sources:
+        past = {}
+        ends = []
+        for step,dir in enumerate(cycle(dirs),1):
+            # print(step,curr,dir)
+            if curr.endswith("Z"):
+                ends.append(step)
+            p = (curr,(step-1)%len(dirs))
+            if p in past: break
+            past[p] = step
+            curr = right[curr] if dir=='R' else left[curr]
+        print(curr,step - past[p],ends)
+        lcm = math.lcm(lcm,step - past[p])
+    print(lcm)
---
+++ Day_9/p2.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+HOME = Path(__file__).parent
+
+with open(HOME/"test.txt") as f:
+    pass
